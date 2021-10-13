2a1fb6cb4b9eb5bde86141e35378b7334e84c33b
zzp <847587146@qq.com>
2021-10-13 09:42:09 +0800

    asd

--- src/component/input/InputSearch.tsx
+++ src/component/input/InputSearch.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { InlintStyleType } from '@/shims'
 
 // const InputStyle = {
 //     boxSizing: 'border-box',
@@ -25,9 +26,34 @@ const InputStyle = styled.input`
     box-sizing: border-box;
 `
 
+const SearchBox = styled.div`
+    position: absolute;
+    background-color: var(--box-background-color);
+    top: 35px;
+    left: 0;
+    width: 100%;
+    z-index: 10;
+`
+
+const boxStyle:InlintStyleType = {
+    padding: '10px',
+    fontSize: '14px',
+    lineHeight: '150%',
+    textAlign: 'left',
+    borderBottom: '1px solid var(--box-border-color)'
+}
+
 const Input = function() {
     return (
-        <InputStyle type="text"></InputStyle>
+        <div>
+            <InputStyle type="text"></InputStyle>
+            <SearchBox>
+                <div style={boxStyle}>
+                    <h3>内容1</h3>
+                    <h3>内容2</h3>
+                </div>
+            </SearchBox>
+        </div>
     )
 }
 
