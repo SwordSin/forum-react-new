a0093dcea6ef82a0517c56f439ca7926532d8f11
Your Name <you@example.com>
2021-12-29 13:06:16 +0800

    富文本 - 中文

--- src/component/editorNew/DraftEdit.tsx
+++ src/component/editorNew/DraftEdit.tsx
@@ -36,6 +36,9 @@ const EditorTest = function(props: any) {
     <div className='editor' name-data={props.name}>
         <Editor
             editorState={editorState}
+            localization={{
+                locale: 'zh'
+            }}
             toolbarClassName="toolbarClassName"
             wrapperClassName="wrapperClassName"
             editorClassName="editorClassName"
